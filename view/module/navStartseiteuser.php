12935c273f39ce0cb8da134e474d2ad2d4a756e8
Thomas Schueler <thomas.schueler@bbqberlin.onmicrosoft.com>
2020-06-25 15:31:54 +0200

    25.06.2020 Ende

---
+++ view/module/navStartseiteuser.php
@@ -0,0 +1,6 @@
+
+
+
+    <tr><td><a href="index.php?action=startseite&area=user">Home</a></td></tr>
+    <tr><td><a href="index.php?action=profilaendern&area=user">Profil ändern</a></td></tr>
+    <tr><td><a href="index.php?action=ausloggen&area=user">Ausloggen</a></td></tr>
