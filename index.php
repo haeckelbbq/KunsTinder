1eebfd5f9301d0ac6034e4fe68948786a71ee981
Thomas Schueler <thomas.schueler@bbqberlin.onmicrosoft.com>
2020-06-25 15:57:22 +0200

    25.06.2020 nach indexerneuerung

--- index.php
+++ index.php
@@ -1,41 +1,51 @@
 <?php
 include 'config.php';
 
-spl_autoload_register(function ($class)
-{
+spl_autoload_register(function ($class) {
     include 'class/' . $class . '.php';
 });
 
 $action = $_REQUEST['action'] ?? 'startseite';
-$area = $_REQUEST['area'] ?? '';
+$area = $_REQUEST['area'] ?? 'anonymous';
 $username = $_REQUEST['username'] ?? '';
+$rolle = $_REQUEST['rolle'] ?? '';
 
 
-$nachname = $_POST['name'] ?? '';
+$nachname = $_POST['nachname'] ?? '';
 $vorname = $_POST['vorname'] ?? '';
 $passwort = $_POST['passwort'] ?? '';
 $passwort2 = $_POST['passwort2'] ?? '';
 $plz = $_POST['plz'] ?? '';
 $ort = $_POST['ort'] ?? '';
 $strassehausnummer = $_POST['strassehausnummer'] ?? '';
-$bewertung = $_POST['name'] ?? '';
-$fehlermeldung = $_POST['name'] ?? '';
+$bildttitel = $_POST['bildtitel'] ?? '';
+$kategorie = $_POST['kategorie'] ?? '';
+$bewertung = $_POST['bewertung'] ?? '';
+$fehlermeldung = $_POST['fehlermeldung'] ?? '';
 
 
-if ($action === 'startseite')
-{
+if ($action === 'startseite') {
     include 'view/startseite.php';
-}
-elseif($action === 'einloggen')
-{
+} elseif ($action === 'einloggen') {
     include 'view/einloggen.php';
-}
-elseif($action === 'einloggenpruefen')
-{
-    $fehlermeldung = User::userEinloggen($username,$passwort);
+} elseif ($action === 'einloggenueberpruefen') {
+    $fehlermeldung = User::userEinloggen($username, $passwort);
     include 'view/einloggen.php';
+} elseif ($action === 'registrieren') {
+    include 'view/registrieren.php';
+//    echo '<pre>';
+//    echo $action;
+//    echo '</pre>';
+} elseif ($action === 'registrierenueberpruefen') {
+    $fehlermeldung = User::userRegistrieren($username, $vorname, $nachname, $plz, $ort, $strassehausnummer, $passwort, $passwort2);
+    include 'view/registrieren.php';
+} elseif ($action === 'usersperren') {
+
+} elseif ($action === 'userloeschen') {
+
+} elseif ($action === 'bildloeschen') {
+
 }
+
 
 ?>
-
-
