d730863f33b0436d3ba1f210090d5db5077bf02f
Thomas Schueler <thomas.schueler@bbqberlin.onmicrosoft.com>
2020-06-29 12:15:52 +0200

    29.06.2020 navbereich

--- view/module/navStartseiteuser.php
+++ view/module/navStartseiteuser.php
@@ -1,6 +1,7 @@
 
 
 
-    <tr><td><a href="index.php?action=startseite&area=user">Home</a></td></tr>
-    <tr><td><a href="index.php?action=profilaendern&area=user">Profil ändern</a></td></tr>
-    <tr><td><a href="index.php?action=ausloggen&area=user">Ausloggen</a></td></tr>
+<tr><td><a href="index.php?action=startseite">Home</a></td></tr>
+<tr><td><a href="index.php?action=profilaendern">Profil ändern</a></td></tr>
+<tr><td><a href="index.php?action=bildhochladendialog">Bild hochladen</a></td></tr>
+<tr><td><a href="index.php?action=ausloggen">Ausloggen</a></td></tr>
