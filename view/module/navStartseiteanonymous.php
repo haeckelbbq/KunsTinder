5ac27f14fd555fa746a54babeea3077f22f465f0
Manon <manonsf@gmail.com>
2020-06-29 14:08:20 +0200

    Neue Anpassungen

--- view/module/navStartseiteanonymous.php
+++ view/module/navStartseiteanonymous.php
@@ -1,7 +1,7 @@
 
 
 
-    <tr><td><a href="index.php?action=startseite&area=user">Home</a></td></tr>
-    <tr><td><a href="index.php?action=registrieren&area=user">registrieren</a></td></tr>
-    <tr><td><a href="index.php?action=einloggen&area=user">Einloggen</a></td></tr>
+    <tr><td><a href="index.php?action=startseite">Home</a></td></tr>
+    <tr><td><a href="index.php?action=registrieren">registrieren</a></td></tr>
+    <tr><td><a href="index.php?action=einloggen">Einloggen</a></td></tr>
 
