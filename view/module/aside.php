c3111708341bfc0383a333a24aff12bba04a846f
Thomas Schueler <thomas.schueler@bbqberlin.onmicrosoft.com>
2020-06-30 09:01:33 +0200

    30.06.2020 suche & aside

--- view/module/aside.php
+++ view/module/aside.php
@@ -1,11 +1,10 @@
 <form method="post" action="index.php">
-
     <input type="hidden" name="action" value="suchen">
     <label for ="Kategorie">Kategorie</label><br>
-    <input id="Kategorie"><br>
+    <input type="hidden" id="Kategorie" name="kategorie"><br>
     <label for ="Kuenstler">Künstler</label><br>
-            <input id="Kuenstler"><br>
+    <input type="hidden" id="Kuenstler" name="username"><br>
     <label for ="Bildtitel">Bildtitel</label><br>
-                <input id="Bildtitel"><br>
+    <input type="hidden" id="Bildtitel" name="bildtitel"><br>
     <button type="submit" name="submitbutton" value="OK">Suchen</button>
 </form>
